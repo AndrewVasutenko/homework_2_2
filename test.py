e4eda6b9ba7574a773d075951c3b9ae4132b98c3
AndrewVasutenko <vasutenko@gmail.com>
2022-09-06 01:43:30 +0200

    Fixed negative test and updated last selene version

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 from selene.support.shared import browser
 from selene import be, have
 import pytest
+
+
 @pytest.fixture(scope="session", autouse=True)
 def default_browser_resolution():
     browser.config.window_width = 800
     browser.config.window_height = 1440
+
+
 def positive_search_test():
     browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
 def negative_test():
     browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('Python').press_enter()
